e7ecf3e1576c9b272b597e39c73cc35e0db6bb02
Karina Valens <karyvalens@yahoo.com>
2022-03-09 11:50:46 +0100

    got the data

--- js/productList.js
+++ js/productList.js
@@ -14,10 +14,28 @@ fetch(url, options)
         return response.json();
     })
     .then((data) => {
-        //we have the data
-        console.log(data);
-        //handleData(data);
+        //to check if we have the data
+        //console.log(data);
+        handleData(data);
     })
     .catch((e) => {
         ("An error ocurred:", e.message);
     })
+
+function handleData(soaps) {
+    soaps.forEach(soap => {
+        //console.log(soap);
+        // 1. Grap the template
+        const template = document.querySelector("template").content;
+
+        // 2. Clone template
+        const clone = template.cloneNode(true);
+        // 4. Populated with data
+        // 5. Grap the parent
+        const parent = document.querySelector("#produtsCont");
+        // 6. Appent to the DOM
+        parent.appendChild(clone);
+
+
+    });
+}
